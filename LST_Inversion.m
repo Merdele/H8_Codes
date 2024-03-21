0b7cc987e642b1748d96fd2ba1c4145759b69650
Merdele <93083395+Merdele@users.noreply.github.com>
2024-03-21 14:37:17 +0800

    finished code to index crater areas only

--- LST_Inversion.m
+++ LST_Inversion.m
@@ -2,14 +2,16 @@
 % The firstly the files are loaded and rearranged into N x 1 columns to
 % allow for Inversion
 % Inversion is then carried out
-% Created by Denny on 13 Feb 2024. 
+% Created by Denny on 13 Feb 2024.
+% CREATED BRANCH FOR CRATER ANALYSIS ONLY. THIS IS FOR CRATER ANALYSIS
+% updated on 21 Mar 2024
 clear
                 
 % add path for the create_aoi_coords function
 addpath '/Users/denny/OneDrive - Nanyang Technological University/Y4/FYP/H8_Codes'
 
 %% This section chooses the volcano of interest
-volcano_name = 'Marapi'; %change this to volcano of interest
+volcano_name = 'Taal'; %change this to volcano of interest
 
 switch volcano_name
 
@@ -67,6 +69,7 @@ volcano_lon_indexing = 0.02 * floor(Volcano_lon / 0.02);
 crater_lon_index = find(Himawari_lon.(fieldname{1})>volcano_lon_indexing-0.01 & ...
 Himawari_lon.(fieldname{1})<volcano_lon_indexing+0.01);
 
+
     if length(fieldname) == 1
 
         T_i = tbb_13.(fieldname{1})(crater_lat_index,crater_lon_index);
@@ -97,13 +100,13 @@ Himawari_lon.(fieldname{1})<volcano_lon_indexing+0.01);
    
     end
 
-    cumulative_T_i = [cumulative_T_i;T_i];
-    cumulative_T_j = [cumulative_T_j;T_j];
-    cumulative_T_k = [cumulative_T_k;T_k];
+    cumulative_T_i = [cumulative_T_i;T_i;T_i;T_i;T_i];
+    cumulative_T_j = [cumulative_T_j;T_j;T_j;T_j;T_j];
+    cumulative_T_k = [cumulative_T_k;T_k;T_k;T_k;T_k];
     
 end
 
-plot(cumulative_T_k)
+
 
 
 %% This section loads the MODIS files
@@ -117,6 +120,8 @@ modis_filename_struct = (dir(fullfile(modis_data_folder,[volcano_name,'*'])));
 
 cumulative_LST = [];
 
+cumulative_lat = [];
+
 for i = start_loop:length(modis_filename_struct)
 
     load([modis_filename_struct(i).name])
@@ -127,59 +132,103 @@ for i = start_loop:length(modis_filename_struct)
     clear('lat','lon')
 
 
-    switch volcano_name
-        case 'Marapi'
-    LST = LST(1:10,:);
-
-    LST(LST == 0) = NaN;
-    
-    test = reshape(LST, [2, size(LST, 1)/2, size(LST, 2)]);
-
-    average_LST = nanmean(test, 1);
-
-    average_LST = squeeze(average_LST);
-
-    resampled_LST = average_LST(:,1:5);
-
-    resampled_LST_column = resampled_LST(:);
-
-    nan_idx = find(resampled_LST_column == 0);
-
-    resampled_LST_column(nan_idx) = [];
-
-    cumulative_LST = [cumulative_LST;resampled_LST_column];
-
-        case {'Sinabung','Taal'}
-    LST = LST(1:15,:);
-
-    LST(LST == 0) = NaN;
-    
-        % Keep the first row
-    average_row_LST = LST(1, :);
-    
-    % Calculate the average of every two of the remaining rows
-    for j = 2:2:size(LST, 1) - 1
-        average_row_LST = [average_row_LST; mean(LST(j:j+1, :), 1)];
-    end
+    volcano_lat_indexing = 0.02 * floor(Volcano_lat / 0.02);
 
 
-    average_row_LST = average_row_LST(:,1:9);
 
-        % Calculate the average of every overlapping two columns
-    average_LST = zeros(size(average_row_LST, 1), size(average_row_LST, 2) - 1);
-
-    for h = 1:size(average_row_LST, 2) - 1
-        average_LST(:, h) = (average_row_LST(:, h) + average_row_LST(:, h + 1)) / 2;
-    end
-
-    resampled_LST_column = average_LST(:);
-
-    cumulative_LST = [cumulative_LST;resampled_LST_column];
+crater_lat_index = find(MODIS_lat>volcano_lat_indexing-0.01 & ...
+MODIS_lat<volcano_lat_indexing+0.01);
 
 
-    end
+volcano_lon_indexing = 0.02 * floor(Volcano_lon / 0.02);
+
+crater_lon_index = find(MODIS_lon>volcano_lon_indexing-0.01 & ...
+MODIS_lon<volcano_lon_indexing+0.01);
+
+
+
+switch length(crater_lon_index)
+
+    case 1
+        switch length(crater_lat_index)
+            case 1
+                LST_indexed = LST(crater_lat_index,crater_lon_index);
+                LST_indexed = LST_indexed(:);
+                cumulative_LST = [cumulative_LST;LST_indexed;LST_indexed;...
+                    LST_indexed;LST_indexed];
+
+            case 2
+                LST_indexed = LST(crater_lat_index,crater_lon_index);
+                LST_indexed = LST_indexed(:);
+                cumulative_LST = [cumulative_LST;LST_indexed;LST_indexed];
+        end 
+    case 2
+        switch length(crater_lat_index)
+            case 1
+        LST_indexed = LST(crater_lat_index,crater_lon_index);
+        LST_indexed = LST_indexed(:);
+        cumulative_LST = [cumulative_LST;LST_indexed;LST_indexed];
+            case 2
+        LST_indexed = LST(crater_lat_index,crater_lon_index);
+        LST_indexed = LST_indexed(:);
+        cumulative_LST = [cumulative_LST;LST_indexed];
+        end 
 end
 
+end
+% 
+% 
+%     switch volcano_name
+%         case 'Marapi'
+%     LST = LST(1:10,:);
+% 
+%     LST(LST == 0) = NaN;
+% 
+%     test = reshape(LST, [2, size(LST, 1)/2, size(LST, 2)]);
+% 
+%     average_LST = nanmean(test, 1);
+% 
+%     average_LST = squeeze(average_LST);
+% 
+%     resampled_LST = average_LST(:,1:5);
+%     resampled_LST_column = resampled_LST(:);
+% 
+%     nan_idx = find(resampled_LST_column == 0);
+% 
+%     resampled_LST_column(nan_idx) = [];
+% 
+%     cumulative_LST = [cumulative_LST;resampled_LST_column];
+% 
+%         case {'Sinabung','Taal'}
+%     LST = LST(1:15,:);
+% 
+%     LST(LST == 0) = NaN;
+% 
+%         % Keep the first row
+%     average_row_LST = LST(1, :);
+% 
+%     % Calculate the average of every two of the remaining rows
+%     for j = 2:2:size(LST, 1) - 1
+%         average_row_LST = [average_row_LST; mean(LST(j:j+1, :), 1)];
+%     end
+% 
+% 
+%     average_row_LST = average_row_LST(:,1:9);
+% 
+%         % Calculate the average of every overlapping two columns
+%     average_LST = zeros(size(average_row_LST, 1), size(average_row_LST, 2) - 1);
+% 
+%     for h = 1:size(average_row_LST, 2) - 1
+%         average_LST(:, h) = (average_row_LST(:, h) + average_row_LST(:, h + 1)) / 2;
+%     end
+% 
+%     resampled_LST_column = average_LST(:);
+% 
+%     cumulative_LST = [cumulative_LST;resampled_LST_column];
+% 
+%     end
+% %end
+% 
 
 %% This section cleans the data
 
@@ -191,8 +240,16 @@ cumulative_T_i(nan_idx) = [];
 cumulative_T_j(nan_idx) = [];
 cumulative_T_k(nan_idx) = [];
 
+% removes the indexes for all variables where O is for LST
+zero_idx = find(cumulative_LST == 0);
+
+cumulative_LST(zero_idx) = [];
+cumulative_T_i(zero_idx) = [];
+cumulative_T_j(zero_idx) = [];
+cumulative_T_k(zero_idx) = [];
+
 %% removes indexes for all variables where BT for Ti,j,k are below 240
-less_than_variable = 290;
+less_than_variable = 270;
 cumulative_T_i_lessthan_idx = find(cumulative_T_i < less_than_variable);
 
 cumulative_LST(cumulative_T_i_lessthan_idx) = [];
@@ -214,6 +271,7 @@ cumulative_T_i(cumulative_T_k_lessthan_idx) = [];
 cumulative_T_j(cumulative_T_k_lessthan_idx) = [];
 cumulative_T_k(cumulative_T_k_lessthan_idx) = [];
 
+%%
 cumulative_LST = cumulative_LST - 273;
 cumulative_T_i = cumulative_T_i - 273;
 cumulative_T_j = cumulative_T_j - 273;
@@ -239,12 +297,12 @@ data = [cumulative_LST, cumulative_T_i, cumulative_T_j, cumulative_T_k];
 R = corrplot(data,'varNames',{'LST','tbb13','tbb14','tbb15'});
 
 %%
-matfilename = (['Results_of_Inversion_',volcano_name,'.mat']);
-
-Output_Folder = (['/Users/denny/OneDrive - Nanyang Technological University/Y4/FYP/LST_Inversion/'...
-    volcano_name,'/']);
-
-% specify which variables to be saved depending on what is to be read.
-save([Output_Folder,matfilename],'mEst','cumulative_LST','cumulative_T_i',...
-    'cumulative_T_j','cumulative_T_k');
+% matfilename = (['Results_of_Inversion_',volcano_name,'.mat']);
+% 
+% Output_Folder = (['/Users/denny/OneDrive - Nanyang Technological University/Y4/FYP/LST_Inversion/'...
+%     volcano_name,'/']);
+% 
+% % specify which variables to be saved depending on what is to be read.
+% save([Output_Folder,matfilename],'mEst','cumulative_LST','cumulative_T_i',...
+%     'cumulative_T_j','cumulative_T_k');
 
