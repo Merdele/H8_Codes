4213993cc8cbcad6c740194bb21e7b6ed487d90f
Merdele <93083395+Merdele@users.noreply.github.com>
2024-04-01 08:54:51 +0800

    Update elev_against_himawari_temp.m

--- elev_against_himawari_temp.m
+++ elev_against_himawari_temp.m
@@ -56,10 +56,6 @@ load(tbbfiletoread)
 % load(NTBfiletoread)
 % load(stackfiletoread)
 
-mkdir([Himawari_Data_Folder,foldername,'/','DEMvsTBB_Night'])
-cd([Himawari_Data_Folder,foldername,'/DEMvsTBB_Night'])
-
-% 
 variableNames = {'tbb_07','tbb_08','tbb_09','tbb_10','tbb_11','tbb_12',...
     'tbb_13','tbb_14','tbb_15','tbb_16'};
 % variableNames = {'BTD_13_14','BTD_13_15','BTD_14_15','BTD_7_13','BTD_7_14',...
@@ -95,10 +91,11 @@ yData = [yData;data(:)];
 end
 yData = yData-273;
 
-greater_than_18_index = yData >=0;
+greater_than = yData >=10;
 
-yData = yData(greater_than_18_index);
-xData = xData(greater_than_18_index);
+yData = yData(greater_than);
+xData = xData(greater_than);
+
 %%
 
 % Perform least squares fit (linear fit in this case)
@@ -111,41 +108,93 @@ one_row_coefficients = {YYYYMMDD,currentVarName,coefficients(1)*1000};
 
 cumulative_coefficients=[cumulative_coefficients;one_row_coefficients];
 
-% figure
-% hold on
-% 
-% % Create a scatter plot
-% scatter(xData, yData);
-% 
-% 
-% % Plot the least squares fit line
-% fitLine = polyval(coefficients, xData);
-% plot(xData, fitLine, 'r-', 'LineWidth', 2);
-% 
-% % % Plot the linear fit line
-% % xFit = linspace(min(xData), max(xData), 100);
-% % yFit = predict(mdl, xFit');
-% % plot(xFit, yFit, 'r-', 'LineWidth', 2, 'DisplayName', 'Linear Fit');
-% 
-% % Customize the plot
-% 
-% title([Volcano,' ',YYYYMM,DD{h},sprintf(' %s Temperature vs Elevation',...
-%     strrep(variableNames{k},'_',' '))]);
-% % title(sprintf('Scatter Plot of Temperature %s vs Elevation',...
-% %     strrep(variableNames{k},'_',' ')));
-% xlabel('Elevation');
-% ylabel('Brightness Temperature (Celsius)');
-% legend('Data Points', 'Least Squares Fit','Location','best');
-% 
-% hold off;
 
-% fig_filename = ([strrep(variableNames{k},'_',' '),' TempvsElev.png']);
-% saveas(gcf, fig_filename);
-% close
+end
+
+end
+
+
+%%
+
+YYYYMM = '201906';
+DD = {'01','02','03','04','05','06','07','08'};
+
+for h = 1:length(DD)
+
+% creating filenames
+foldername = [Volcano,'_',YYYYMM,'/',Volcano,'_',YYYYMM,DD{h},'_',DayNight];
+% foldername = [Volcano,'_2019'];
+tbbfilename = [Volcano,'_',YYYYMM,DD{h},'_',DayNight,'.mat'];
+% btdfilename = [Volcano,'_',YYYYMM,DD{h},'_',DayNight,'_BTD.mat'];
+% NTBfilename = [Volcano,'_',YYYYMM,DD,'_',DayNight,'_NTB.mat'];
+%stackfilename = [Volcano,'_',YYYYMM,DD,'_',DayNight,'_Stacked.mat'];
+
+% combining filenames and paths
+DEMfiletoread = ([DEM_Data_Folder,DEMFileName]);
+% btdfiletoread = ([Himawari_Data_Folder,'/',foldername,'/',btdfilename]);
+tbbfiletoread = ([Himawari_Data_Folder,'/',foldername,'/',tbbfilename]);
+% NTBfiletoread = ([Matfile_Data_Folder,'/',foldername,'/',NTBfilename]);
+%stackfiletoread = ([Matfile_Data_Folder,'/',foldername,'/',stackfilename]);
+
+[DEM,R] = readgeoraster(DEMfiletoread,"OutputType","double");
+% load(btdfiletoread)
+load(tbbfiletoread)
+% load(NTBfiletoread)
+% load(stackfiletoread)
+
+
+variableNames = {'tbb_07','tbb_08','tbb_09','tbb_10','tbb_11','tbb_12',...
+    'tbb_13','tbb_14','tbb_15','tbb_16'};
+% variableNames = {'BTD_13_14','BTD_13_15','BTD_14_15','BTD_7_13','BTD_7_14',...
+% 'BTD_7_15'};
+
+%%
+
+for k = 1:length(variableNames)
+
+currentVarName = variableNames{k};
+
+currentVarValue = evalin('base', currentVarName);
+
+myStruct = currentVarValue;
+
+fieldName = fieldnames(myStruct);
+
+%% This section combines the data in the structs into a single variable.
+% the combined data can be controlled for now by looking at the the
+% fieldName 
+
+xData = [];
+yData = [];
+
+for i = 1:length(fieldName)
+data = myStruct.(fieldName{i});
+
+% repeatedly combines the DEM values. Position of the DEM values
+% corresponds to the positions of the data values.
+xData = [xData;DEM(:)]; 
+yData = [yData;data(:)];
+
+end
+yData = yData-273;
+
+yData = yData(greater_than);
+xData = xData(greater_than);
+%%
+
+% Perform least squares fit (linear fit in this case)
+degree = 1;  % Degree of the polynomial (1 for linear fit)
+coefficients = polyfit(xData, yData, degree);
+
+YYYYMMDD = [YYYYMM,DD{h}];
+
+one_row_coefficients = {YYYYMMDD,currentVarName,coefficients(1)*1000};
+
+cumulative_coefficients=[cumulative_coefficients;one_row_coefficients];
 
 
 Output_Folder = ([Himawari_Data_Folder,'/',Volcano,'_',YYYYMM]);
-matfilename = [Volcano,'_',YYYYMM,'_',DayNight,'_coefficients_0.mat'];
+matfilename = [Volcano,'_',YYYYMM,'_',DayNight,'_coefficients_10.mat'];
 
 % specify which variables to be saved depending on what is to be read.
 save([Output_Folder,'/',matfilename],"cumulative_coefficients")
